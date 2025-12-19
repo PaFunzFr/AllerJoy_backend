3144e05a5797ea27914c7d77018b904cb86ef921
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-12-19 09:54:54 +0100

    Update group model

--- app_groups/models.py
+++ app_groups/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Group(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
+    date = models.DateTimeField(null=True, blank=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
